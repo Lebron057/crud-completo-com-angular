cb72d61c5c5ab78caf77bcde5abc5e32b76bcae8
Lebron057 <iagoamorim057@gmail.com>
2025-11-23 19:30:26 -0300

    etapa 6

--- src/app/components/posts-delete/posts-delete.ts
+++ src/app/components/posts-delete/posts-delete.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Post, PostsServices } from '../../services/posts-services';
+import { Toast } from '../../services/toast';
 
 @Component({
   selector: 'app-posts-delete',
+  standalone: true,
   imports: [],
   templateUrl: './posts-delete.html',
-  styleUrl: './posts-delete.css',
+  styleUrl: './posts-delete.css' // Pode manter vazio ou remover se não usar
 })
 export class PostsDelete {
+  activeModal = inject(NgbActiveModal);
+  private postService = inject(PostsServices);
+  private toastService = inject(Toast);
 
+  // Recebe o post que se deseja excluir
+  @Input() post!: Post;
+
+  confirmDelete() {
+    // Chama o serviço de delete passando o ID
+    this.postService.delete(this.post.id!).subscribe({
+      next: () => {
+        this.toastService.showSuccess('Post excluído com sucesso!');
+        
+        // Retorna o ID do post para que a lista saiba qual card remover
+        this.activeModal.close(this.post.id); 
+      },
+      error: (err) => {
+        console.error(err);
+        this.toastService.showDanger('Erro ao excluir o post.');
+      }
+    });
+  }
 }

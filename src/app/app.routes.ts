3498a5e2ae1e6e4fbc46dee8233c6cf692beb75f
Lebron057 <iagoamorim057@gmail.com>
2025-11-23 15:54:39 -0300

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { PostsList } from './components/posts-list/posts-list';
+
+export const routes: Routes = [
+    {
+        path: '', component: PostsList
+    }
+];
---
+++ src/app/components/posts-list/posts-list.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PostsCreate } from '../posts-create/posts-create';
+import { PostsUpdate } from '../posts-update/posts-update';
+import { PostsDelete } from '../posts-delete/posts-delete';
+
+
+@Component({
+  selector: 'app-posts-list',
+  imports: [],
+  templateUrl: './posts-list.html',
+  styleUrl: './posts-list.css',
+})
+export class PostsList {
+  private readonly modalService = inject(NgbModal);
+
+  openModalCreate() {
+    this.modalService.open(PostsCreate, { size: 'lg' });
+  }
+
+  openModalUpdate() {
+    this.modalService.open(PostsUpdate, { size: 'lg' });
+  }
+
+  openModalDelete() {
+    this.modalService.open(PostsDelete, { size: 'lg' });
+  }
+  
+}
